35897abc804da2f68046ba176b76fc135cbbbc53
vikassrivastava123 <vikassrivastava123@gmail.com>
2022-09-11 17:30:50 +0530

    Update sloths.sol

--- sloths.sol
+++ sloths.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.4;
 import "ERC721r.sol";
 contract Sloths is ERC721r {
     // 1601 is the number of tokens in the colletion
-    uint256 TOTAL_SUPPLY = 1600;
+    uint256 TOTAL_SUPPLY = 8400;
     string public baseURI = "https://gateway.pinata.cloud/ipfs/QmVbDfDcEWP7oLjd28cLEXJ8iATT4C6MuyVqwPs6QXhJSN";
 
-    constructor() ERC721r("HHC Sloths", "SLOTH", 8400) {}
+    constructor() ERC721r("HHC Sloths", "SLOTH", TOTAL_SUPPLY) {}
     
     function mint (uint quantity) external payable {
         _mintRandom(msg.sender, quantity);
