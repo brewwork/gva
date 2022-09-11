85c6ad14bc069aa8e830a3f2db8abe1fd57effe6
vikassrivastava123 <vikassrivastava123@gmail.com>
2022-09-11 18:06:38 +0530

    test

--- hawks.sol
+++ hawks.sol
@@ -4,13 +4,16 @@ pragma solidity ^0.8.4;
 import "ERC721r.sol";
 
 contract Hawks is ERC721r {
-    // 1601 is the number of tokens in the colletion
-    uint256 TOTAL_SUPPLY = 1600;
+    // 1600 is the number of tokens in the colletion
+    uint256 MAX_SUPPLY = 1600;
+    uint256 MAX_MINTS = 10;
     string public baseURI = "https://gateway.pinata.cloud/ipfs/QmVbDfDcEWP7oLjd28cLEXJ8iATT4C6MuyVqwPs6QXhJSN";
 
-    constructor() ERC721r("HHC Hawks", "HAWK", TOTAL_SUPPLY) {}
+    constructor() ERC721r("HHC Hawks", "HAWK", MAX_SUPPLY) {}
     
     function mint (uint quantity) external payable {
+        require(quantity <= MAX_MINTS, "Not allowed to mint more than limit");
+        require(totalSupply() + quantity <= MAX_SUPPLY, "Not enough tokens left");
         _mintRandom(msg.sender, quantity);
     }
 
--- sloths.sol
+++ sloths.sol
@@ -3,13 +3,15 @@ pragma solidity ^0.8.4;
 
 import "ERC721r.sol";
 contract Sloths is ERC721r {
-    // 1601 is the number of tokens in the colletion
-    uint256 TOTAL_SUPPLY = 8400;
+    // 8400 is the number of tokens in the colletion
+    uint256 MAX_SUPPLY = 8400;
+    uint256 MAX_MINTS = 10;
     string public baseURI = "https://gateway.pinata.cloud/ipfs/QmVbDfDcEWP7oLjd28cLEXJ8iATT4C6MuyVqwPs6QXhJSN";
 
-    constructor() ERC721r("HHC Sloths", "SLOTH", TOTAL_SUPPLY) {}
-    
+    constructor() ERC721r("HHC Sloths", "SLOTH", MAX_SUPPLY) {}
     function mint (uint quantity) external payable {
+        require(quantity <= MAX_MINTS, "Not allowed to mint more than limit");
+        require(totalSupply() + quantity <= MAX_SUPPLY, "Not enough tokens left");
         _mintRandom(msg.sender, quantity);
     }
 
